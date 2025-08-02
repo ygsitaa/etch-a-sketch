04518b8fb5d6332426fb8c106594c79a465a3d3e
Daniel Hodiamont <213696653+ygsitaa@users.noreply.github.com>
2025-08-02 16:09:57 +0200

    Added "hover" mechanic (3rd step in exercise)

--- eas.js
+++ eas.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container");
 const boxes16 = 16 * 16;
 const size16 = (100 / 16).toFixed(5);
 
-const boxes = document.querySelectorAll(".single-box")
+
 for (let i = 0; i < boxes16; i++) {
     const box = document.createElement("div");
     box.classList.add("single-box");
@@ -11,7 +11,12 @@ for (let i = 0; i < boxes16; i++) {
     container.appendChild(box);
 };
 
+const boxes = document.querySelectorAll(".single-box")
 
+boxes.forEach(box => {
+    box.addEventListener("mouseover", function() {
+        box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+    });
+});
 
 //const boxesPrompt = prompt("How many boxes do you want per row?");
-// background-color: rgb(${randomRgb}, ${randomRgb}, ${randomRgb})
